9a1ab83b0d3f6602e3461817516a8bf1b40084c4
Keenan Sturtevant <keenansturtevant@gmail.com>
2015-10-24 23:02:12 -0500

    please god, this is it I know it

--- app/controllers/code_scan_controller.rb
+++ app/controllers/code_scan_controller.rb
@@ -7,12 +7,7 @@ class CodeScanController < ActionController::Base
   def scan_code_post
     render text: "Success"
     puts "!" * 100
+    Unit.new(unit_number: params[:unit_number]).save
 
-    respond_to do |format|
-      format.html { puts " html " }
-      format.js    { puts " js "}
-      format.xml  { puts "xml" }
-    end
   end
-
 end
