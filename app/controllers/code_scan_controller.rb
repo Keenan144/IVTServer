c2ddd1b9627f0ff3a612e9a21212106a1e621a75
Keenan Sturtevant <keenansturtevant@gmail.com>
2015-10-24 22:08:01 -0500

    render json

--- app/controllers/code_scan_controller.rb
+++ app/controllers/code_scan_controller.rb
@@ -1,9 +1,11 @@
 class CodeScanController < ActionController::Base
   def scan_code_get 
     render text: "Success"
+    render json: params
   end
 
   def scan_code_post
     render text: "Success"
+    render json: params
   end
 end
