314a282616f2d95382f1bee3cd7758170a20bc67
Keenan Sturtevant <keenansturtevant@gmail.com>
2015-10-25 13:43:22 -0500

    parsed longlat

--- app/controllers/code_scan_controller.rb
+++ app/controllers/code_scan_controller.rb
@@ -7,7 +7,9 @@ class CodeScanController < ActionController::Base
   def scan_code_post
     render text: "Success"
     puts "!" * 100
-    Unit.new(unit_number: params[:unit_number], longitude: params[:longitude], username: params[:username], address: params[:address]).save
+    old_longLat = params[:longitude].to_string
+    @longLat = old_longLat[21,22]
+    Unit.new(unit_number: params[:unit_number], longitude: @longLat, username: params[:username], address: params[:address]).save
 
   end
 end
